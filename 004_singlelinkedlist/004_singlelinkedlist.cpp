244a2ed0c66db066402b794f98c7e6477ce6e21a
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-18 17:49:02 +0700

    mengisi node dengan memasukkan nim node sebelumnya dan yang sedang digunakan

--- 004_singlelinkedlist/004_singlelinkedlist.cpp
+++ 004_singlelinkedlist/004_singlelinkedlist.cpp
@@ -49,3 +49,25 @@ void addNode() {
 	previous->next = nodeBaru;
 }
 
+bool serachNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
