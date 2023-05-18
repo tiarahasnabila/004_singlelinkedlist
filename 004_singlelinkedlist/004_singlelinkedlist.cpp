5d7615201879606eae05a8fd4e5990224cf5eea7
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-18 17:43:29 +0700

    menambahkan node baru dan memasukkan nim  dan nama kedalam node yang sedang digunakan

--- 004_singlelinkedlist/004_singlelinkedlist.cpp
+++ 004_singlelinkedlist/004_singlelinkedlist.cpp
@@ -7,3 +7,14 @@ struct Node {
 	Node* next;
 };
 Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM : ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
