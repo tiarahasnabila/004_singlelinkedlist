e08756013666449ebdeab9119ebd56a762ee9f40
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-18 17:47:29 +0700

    mengecek node

--- 004_singlelinkedlist/004_singlelinkedlist.cpp
+++ 004_singlelinkedlist/004_singlelinkedlist.cpp
@@ -31,4 +31,18 @@ void addNode() {
 		return;
 	}
 
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
 	
